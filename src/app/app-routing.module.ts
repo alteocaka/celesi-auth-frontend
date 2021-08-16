4a7ebd1893c14e786df94caaade376f7c3ac5b9b
Alteo Caka <alteo.caka@gmail.com>
2021-08-16 11:14:09 +0200

    Local changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LayoutModule } from './layout/layout.module';
+import { LoginLayoutComponent } from './layout/login-layout/login-layout.component';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
+    children: [],
+  },
+  {
+    path: 'login',
+    component: LoginLayoutComponent,
     children: [],
   }
 ];
